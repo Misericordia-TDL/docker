ec3cda3d798bf68440dfaacf01dbc7d7ec97e40e
Orlando Méndez <omendezmorales@users.noreply.github.com>
2017-02-04 16:34:23 -0300

    Added a conditional to identify the OS
    Since other OS can be used, added a few lines to check which OS are we working on.

--- develop.sh
+++ develop.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
 # Set environment variables for dev
-export XDEBUG_HOST=$(ipconfig getifaddr en0) # Specific to host
+unamestr=`uname`
+if [[ "$unamestr" == 'Linux' ]]; then
+        export XDEBUG_HOST=$(ifconfig en0  | grep 'inet ' | awk '{print $2}')
+else  #assuming the OS is MacOS
+        export XDEBUG_HOST=$(ipconfig getifaddr en0) # Specific to host
+fi
 
 if [ $# -gt 0 ]; then
     docker-compose "$@"
